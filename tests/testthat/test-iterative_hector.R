4a90fb256a84ae1669647b051bb9f5ed271a6c0f
Joe Brown <joseph.brown.k@gmail.com>
2022-10-24 14:53:37 -0400

    adding testing code for metric_calc and metric_cal_1run
    Adding/ editing testing code for metric_calc_1run and the metric_calc function.
    
    The tests are super simple and only test that the result output maintains the correct structure and length.

--- tests/testthat/test-iterative_hector.R
+++ tests/testthat/test-iterative_hector.R
@@ -1,5 +1,29 @@
-test_that("lognorm works", {
+test_that("lognorm returns two numeric values", {
   x <- lognorm(1, 0.1)
+
   expect_equal(length(x), 2)
   expect_true(is.numeric(x))
+
 })
+
+# metric_calc_1run test set-up
+ssp245 <- newcore(system.file("input/hector_ssp245.ini", package = "hector"))
+r <- iterative_hector(ssp245,
+                      "global_tas",
+                      2000:2100,
+                      10)
+
+test_that("metric_calc_1run returns a single numeric value", {
+  x <- metric_calc_1run(r,
+                        mean,
+                        "global_tas",
+                        2000:2100)
+
+  # is value length equal to one
+  expect_equal(length(x), 1)
+
+  # is value numeric
+  expect_true(is.numeric(x))
+
+})
+
