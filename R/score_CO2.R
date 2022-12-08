1ccc1c22b580fa63aac115fd5f5774b57072130d
Joe Brown <joseph.brown.k@gmail.com>
2022-12-08 14:42:39 -0500

    building score_ramp func and editing efficiency of  score _CO2

--- R/score_CO2.R
+++ R/score_CO2.R
@@ -1,3 +1,45 @@
+# compute vector of score for two numeric vectors (x and y)
+# w1 - distance at which score ramps down from 1.
+# w2 - distance at which score becomes 0.
+
+#' Computing vector of score for numeric vectors
+#'
+#' @param x First set of numeric vectors
+#' @param y Second set of numeric vectors
+#' @param w1 Distance at which score ramps down from 1.
+#' @param w2 Distance at which score becomes 0
+#'
+#' @return
+#' @export
+#'
+#' @examples
+score_ramp <- function(x, y, w1, w2) {
+
+  stopifnot(w1 >= 0)
+
+  stopifnot(w2 >= w1)
+
+  stopifnot(length(x) == length(y))
+
+  abs_diffs <- abs(x - y)
+
+  scores <- rep(NA_real_, length(x))
+
+  # the order of the following two statements matters, because
+  # we want a diff of w1 to get a score of 1, even when w1 = w2
+  scores [abs_diffs >= w2] <- 0
+  scores [abs_diffs <= w1] <- 1
+
+  # for abs_diffs between w1 and w2 - compute how far between w1 and w2
+  between_w1_w2 <- abs_diffs > w1 & abs_diffs < w2
+  w1_w2_frac <- (abs_diffs [between_w1_w2] - w1) / (w2 - w1)
+
+  scores [between_w1_w2] <- 1 - w1_w2_frac
+
+  return(scores)
+
+}
+
 # Goal: score hector runs based on divergence of CO2 projections in relation to
 # observed values (using mlo data)
 
@@ -17,32 +59,18 @@ score_CO2 <- function(x, w1 = 2, w2 = 20) {
   # subset to include years for CO2 screening
   res_subset <- subset(x, year >= 1959 & year <= 2021)
 
-  # merge hector reuslts with mlo observed CO2 data
+  # merge hector results with mlo observed CO2 data
   res_merge <- merge(x, mlo, by.x = 'year', by.y = 'year')
 
-  # calculate differences between observed and predicted CO2
-  CO2_diff <- abs(res_merge$mean - res_merge$value)
+  # add new column to res_merge computing scores so that we can
+  # calculate mean scores for each run.
+  res_merge$scores <- score_ramp(res_merge$mean, res_merge$value, w1, w2)
 
-  # establishing scoring bounds
-  scores <- rep(NA, length(CO2_diff))
-  scores [CO2_diff <= w1] <- 1
-  scores [CO2_diff >= w2] <- 0
-
-  # establishing a df of CO2_difference, score, and run number
-  score <- as.data.frame(scores)
-  score$scores <- scores
-  score$run_number <- res_merge$run_number
-
-  # split score df by run_number
-  score_split <- split(score, score$run_number)
-
-  # calculate mean for each run_number group
-  score_mean <- sapply(score_split, mean)
+  score_mean <- aggregate(scores ~ run_number,data = res_merge, FUN = mean)
 
   # df of mean scores
-  mean_score <- as.data.frame(do.call("rbind", score_mean))
+  mean_score <- as.data.frame(score_mean)
 
   return(mean_score)
 
 }
-
