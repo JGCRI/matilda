5ce2acd95ef0a0aa2e63784907c607ed12d2ae21
Joe Brown <joseph.brown.k@gmail.com>
2023-01-20 15:23:52 -0500

    Editing hector example data to fix example errors during checks

--- data-raw/computing_example_hector_result.R
+++ data-raw/computing_example_hector_result.R
@@ -5,11 +5,11 @@ core <- newcore(ini)
 # generating params, creating a new metric, and running Hector with
 # param uncertainty
 # the new metric includes all variables in the output (var = NULL)
-params <- generate_params(10)
-metric_example <- new_metric(var = c(GLOBAL_TAS(), ATMOSPHERIC_CO2()),
+params <- generate_params(core, 10)
+metric_example <- new_metric(var = c(GLOBAL_TAS(), CONCENTRATIONS_CO2()),
                              years = 1960:2100,
                              op = mean)
-hector_result <- iterate_hector(core, metric, params)
+hector_result <- iterate_hector(core, metric_example, params)
 
 # Adding the new example Hector output as a data set in data/
 usethis::use_data(hector_result, overwrite = TRUE)
