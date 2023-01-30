8cfff711d9e81ef980b052db51c850f8423f1bb1
Joe Brown <joseph.brown.k@gmail.com>
2023-01-30 16:56:14 -0500

    Updating example hector data for documentation

--- data-raw/computing_example_hector_result.R
+++ data-raw/computing_example_hector_result.R
@@ -2,14 +2,9 @@
 ini <- system.file("input/hector_ssp245.ini", package = "hector")
 core <- newcore(ini)
 
-# generating params, creating a new metric, and running Hector with
-# param uncertainty
-# the new metric includes all variables in the output (var = NULL)
+# generating params and running Hector with param uncertainty
 params <- generate_params(core, 10)
-metric_example <- new_metric(var = GLOBAL_TAS(),
-                             years = 1960:2100,
-                             op = mean)
-hector_result <- iterate_hector(core, metric_example, crit_co2_obs(), params)
+hector_result <- iterate_hector(core, params)
 
 # Adding the new example Hector output as a data set in data/
 usethis::use_data(hector_result, overwrite = TRUE)
