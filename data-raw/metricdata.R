e79546d84e0b555375255a7e5c97ca9e66c52cc6
Joe Brown <joseph.brown.k@gmail.com>
2023-05-22 12:10:24 -0400

    Adding hadcrut5 temp anomaly to internal data

--- data-raw/metricdata.R
+++ data-raw/metricdata.R
@@ -11,9 +11,20 @@ colnames(metricdata_co2)[colnames(metricdata_co2) == "mean"] = "co2_ppm"
 # remove 'unc' (standard deviation) column
 metricdata_co2$unc <- NULL
 
-## HADCRUT data as the observed temperature values for scoring Hector runs.
-##
+## HADCRUT data as observed temperature anomaly values for scoring Hector runs.
 
+## Temperature anomaly data were downloaded on 05/22/2023 from
+## https://www.metoffice.gov.uk/hadobs/hadcrut5/data/current/download.html
+
+# Reading in HADCRUT5 global temmperature anomaly data
+metricdata_tas <- read.csv("data-raw/temp_anomaly_annmean_hadcrut5.csv")
+
+# rename 'anomaly' to anomaly_C' to give unit information
+colnames(metricdata_tas)[colnames(metricdata_tas) == 'anomaly'] = "anomaly_C"
+
+# remove confidece bounds in df - leaving only year and mean anomaly cols
+metricdata_tas$Lower.confidence.limit..2.5.. <- NULL
+metricdata_tas$Upper.confidence.limit..97.5.. <- NULL
 
 # Add metric data for internal use
-usethis::use_data(metricdata_co2, overwrite = TRUE, internal = TRUE)
+usethis::use_data(metricdata_co2, metricdata_tas, overwrite = TRUE, internal = TRUE)
