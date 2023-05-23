caca50fa4b7c764d03b6cb49a8278f731f7e7b18
Joe Brown <joseph.brown.k@gmail.com>
2023-05-22 21:26:16 -0400

    editing score_ramp test code - final round
    Testing using matrices.
    
    Checking error assertions.
    functionality of the function.
    making sure the correct values are computed.

--- tests/testthat/test-score_ramp.R
+++ tests/testthat/test-score_ramp.R
@@ -1,8 +1,5 @@
-# testing df
-r <- data.frame(x = rep(1,3), y = 1:3)
-
 # sample matrix
-m <- matrix(data = 1:15, nrow = 5, ncol = 3)
+m <- matrix(c(rep(1, 6), rep(2, 3), rep(3, 3)), nrow = 3, ncol = 4)
 
 # df with NAs - for entire variable
 m_2row <- matrix(data = c(1:2), nrow = 2, ncol = 2)
@@ -30,7 +27,7 @@ test_that("function stops and produces error messages", {
                regexp = "No non-NA values in observed")
 
   # error when one row in m is NA
-  expect_error(score_ramp(m = m_NA_dat, w1 = 0, w2 = 2),
+  expect_error(score_ramp(m_NA_dat, w1 = 0, w2 = 2),
                regexp = "No non-NA values in modeled")
 
 })
@@ -39,27 +36,30 @@ test_that("function stops and produces error messages", {
 
 test_that("scores assessed correctly based on w1 and w2 values", {
 
-  # when w1 = 0 -- only diffs = 0, score = 1; diffs > 0, score = 0
-  expect_equal(score_ramp(x = r$x, y = r$y, w1 = 0, w2 = 1), c(1, 0, 0))
+  # when w1 is 0 - only diffs equal to 0, score = 1; diffs greater than 0,
+  # score = 0
+  expect_equal(score_ramp(m, w1 = 0, w2 = 1), c(1, 0, 0))
 
-  # when w1 & w2 both = 1 -- diffs <= 1, score = 1; diffs > 1, score = 0
-  expect_equal(score_ramp(x = r$x, y = r$y, w1 = 1, w2 = 1), c(1, 1, 0))
+  # when w1 & w2 both = 1 - diffs less than or equal to 1, score = 1;
+  # diffs greater than 1, score = 0
+  expect_equal(score_ramp(m, w1 = 1, w2 = 1), c(1, 1, 0))
 
-  # when w1 and w2 = 0
-  expect_equal(score_ramp(x = r$x, y = r$y, w1 = 0, w2 = 0), c(1, 0, 0))
+  # when w1 & w2 = 0 - all diffs > only diffs equal to 0 score = 1
+  expect_equal(score_ramp(m, w1 = 0, w2 = 0), c(1, 0, 0))
 
   })
 
 # Testing score calculation
 
-test_that("x-y differences between w1 & w2 are computed accurately", {
+test_that("differences between w1 & w2 are computed accurately", {
 
-  # when x-y diff is between w1 & w2 expect value between 0-1
-  expect_gt(score_ramp(x = 2, y = 1, w1 = 0, w2 = 2), 0)
-  expect_lt(score_ramp(x = 2, y = 1, w1 = 0, w2 = 2), 1)
+  # when diff is between w1 & w2 expect value between 0-1
+  # Here, difference in [2] = 0.5
+  expect_gt(score_ramp(m, w1 = 0, w2 = 2)[2], 0)
+  expect_lt(score_ramp(m, w1 = 0, w2 = 2)[2], 1)
 
-  # when x-y diff is between w1 & w2 expect computed score to equal
+  # when diff is between w1 & w2 expect computed score to equal
   # 1 - (abs_diff - w1) / (w2-w1)
-  expect_equal(score_ramp(x = 2, y = 1, w1 = 0, w2 = 2), 1 - (abs(2 - 1) - 0) / (2 - 0))
+  expect_equal(score_ramp(m, w1 = 0, w2 = 2)[2], 1 - (abs(1 - 2) - 0) / (2 - 0))
 
 })
