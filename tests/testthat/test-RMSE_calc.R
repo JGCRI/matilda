d42fecd9a92da693c5bd71a53bb4f7c11b76c40b
Joe Brown <joseph.brown.k@gmail.com>
2023-06-07 12:26:30 -0400

    Adding test code for RMSE_calc

---
+++ tests/testthat/test-RMSE_calc.R
@@ -0,0 +1,15 @@
+# test RMSE functionality
+
+test_that("RMSE is computed accurately", {
+
+  expect_equal(RMSE_calc(1, 5), sqrt(mean(1 - 5) ^ 2))
+
+  })
+
+# test return is vector
+
+test_that("RMSE return is a vector", {
+
+  expect_vector(RMSE_calc(x = 1:3, y = 3:5))
+
+})
