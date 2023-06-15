e210c0a0c00c890cf4a39114469520f59bf096de
Joe Brown <joseph.brown.k@gmail.com>
2023-06-14 21:09:38 -0400

    Update test-score_bayesian

--- tests/testthat/test-score_bayesian.R
+++ tests/testthat/test-score_bayesian.R
@@ -11,7 +11,35 @@ m_NA_dat <- matrix(data = c(1:5, rep(NA, 15)), nrow = 5, ncol = 4)
 # NA present in model data
 m_NA_single_case <- matrix(data = c(1:3, 2, NA, 4, 3:5, 4:6), nrow = 3, ncol = 4)
 
-score_bayesian(m_NA_single_case, F)
+# Testing error/warning cases
 
-score_bayesian(m, T)
-score_bayesian(m)
+test_that("function stops and produces error messages", {
+
+  # error when matrix has less than two columns
+  expect_error(score_bayesian(m_2row),
+               regexp = "More than 2 columns must be included in input matrix")
+
+  # error when entire m is NA
+  expect_error(score_bayesian(m_NA_obs),
+               regexp = "No non-NA values in observed data")
+
+  # error when one row in m is NA
+  expect_error(score_bayesian(m_NA_dat),
+               regexp = "NAs detected in data. Analysis halted to prevent bad result.")
+
+  # error when an NA is present anywhere in matrix array
+  expect_error(score_bayesian(m_NA_single_case),
+               regexp = "NAs detected in data. Analysis halted to prevent bad result.")
+
+})
+
+# testing output accuracy
+
+test_that("scores assessed correctly", {
+
+  m2 = matrix(data = c(1, 1, 1), nrow = 1, ncol = 3)
+
+  # score uniform when
+  expect_equal(score_bayesian(m2), )
+
+})
