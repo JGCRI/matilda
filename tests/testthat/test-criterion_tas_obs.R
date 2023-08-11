ca9c499fecb0f5fd56a87d093506f738fcae3aa4
Joe Brown <joseph.brown.k@gmail.com>
2023-08-11 15:05:46 -0400

    Re-naming temperature criterion

--- tests/testthat/test-criterion_tas_obs.R
+++ tests/testthat/test-criterion_tas_obs.R
@@ -1,11 +1,11 @@
 # creating new_metric object for tests
-tas_crit <- criterion_tas_obs()
+gmst_crit <- criterion_gmst_obs()
 
 # Testing attribute class
 
 test_that("result has correct attribute class", {
 
-  expect_s3_class(tas_crit, "criterion")
+  expect_s3_class(gmst_crit, "criterion")
 
 })
 
@@ -13,11 +13,11 @@ test_that("result has correct attribute class", {
 
 test_that("output has correct data structure", {
 
-  expect_true(is.integer(c(tas_crit$year)))
+  expect_true(is.integer(c(gmst_crit$year)))
 
-  expect_true(is.character(c(tas_crit$var)))
+  expect_true(is.character(c(gmst_crit$var)))
 
-  expect_true(is.numeric(c(tas_crit$obs_values)))
+  expect_true(is.numeric(c(gmst_crit$obs_values)))
 
 })
 
