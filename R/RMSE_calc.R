5b58e4f81bf7de3bdee78e69760b650e98610431
Joe Brown <joseph.brown.k@gmail.com>
2024-02-06 13:03:10 -0500

    Update to RMSE_calc function adding sigma argument to apply error to calculation
    Adding sigma argument to the function. Sigma will communicate how to incorporate error terms to the RMSE calculation - a constant error term will be applied if a single value is provided. A time-varying error term will be applied if a vector is provided (vector mu = length of observed data).

--- R/RMSE_calc.R
+++ R/RMSE_calc.R
@@ -5,6 +5,9 @@
 #'
 #' @param x A vector of modeled data values
 #' @param y A vector of observed data values
+#' @param sigma A single value or vector of error terms the same length as y.
+#' A single value will apply a constant error term. User can provide a vector
+#' of error terms to incorporate time-varying error into the RMSE calculation.
 #'
 #' @return Returns a vector of RMSE values
 #' @export
@@ -13,15 +16,26 @@
 #' x <- c(1:5)
 #' y <- c(5:9)
 #'
+#' # constant error
 #' RMSE_calc(x, y)
-RMSE_calc <- function(x, y) {
+RMSE_calc <- function(x, y, sigma) {
   # Check if all values are NA
   if (all(is.na(x)) || all(is.na(y))) {
     return(NA)
   }
 
+  # set sigma to default if not provided by the user
+  if( is.null(sigma)) {
+    sigma = sd(y) # if sigma is NULL compute as sd of observed data (y)
+  } else {
+    # if user provides sigma values it should be length = 1 or same length as the obs data.
+    required_length <- length(y)
+    if(length(sigma) != 1 && length(sigma) != required_length)
+      stop(paste("Length of sigma must be a single value or a vector matching the length of ", required_length, ".", sep = ""))
+  }
+
   # compute RMSE
-  rmse_vals <- sqrt(mean((x - y)^2))
+  rmse_vals <- sqrt(mean((x - y)^2) / sigma)
 
   # return a vector of RMSE values
   return(rmse_vals)
