1e152e09217eb5a5c5ed8539fafa7891088b36be
Joe Brown <joseph.brown.k@gmail.com>
2024-02-13 10:04:42 -0500

    Annotating quiet messaging code

--- R/hush.R
+++ R/hush.R
@@ -1,20 +1,30 @@
 # Quiet messages
 
+# Set an option to control whether messages and warning are suppressed.
+# Defaults to verbose messaging.
 options("matilda.verbose" = FALSE)
 
+# Function to suppress messages
 matilda_message <- function(...) {
+  # Check if the verbose option is set to FALSE
   if (getOption("matilda.verbose", default = FALSE)) {
+    # If FALSE, exit function without displaying message
     return()
   }
+  # If verbose option is TRUE, display message
   message(...)
 }
 
 
 # Quiet warnings
 
+# Function to supress messages
 matilda_warning <- function(...) {
+  # Check if the vebise oiption if set to FALSE
   if (getOption("matilda.verbose", default = FALSE)) {
+    # If FALSE, exit the function without displayign message
     return()
   }
+  # If verbose option is TRUE, display message
   warning(...)
 }
